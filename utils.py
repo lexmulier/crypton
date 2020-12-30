88d88d028d4092f6f868474d7ba73c7e4ea5a9fd
Lex Haitsma Mulier <lhaitsmamulier@visualfabriq.com>
2020-12-30 14:43:23 +0100

    managing connection errros

---
+++ utils.py
@@ -0,0 +1,38 @@
+from time import sleep
+
+import ccxt
+
+
+def handle_bad_requests(max_retries=3, sleep_between_retry=True):
+    def decorator(func):
+        def decorated_function(exchange, *args, **kwargs):
+            retries = 0
+
+            while True:
+                try:
+                    return func(exchange, *args, **kwargs)
+                except ccxt.NetworkError as e:
+                    error = e
+                    retries += 1
+                except ccxt.ExchangeError as e:
+                    error = e
+                    retries += 1
+                except Exception as e:
+                    error = e
+                    retries += 1
+
+                if retries > max_retries:
+                    raise error
+
+                print(
+                    "Exchange {} Error {}. Retrying {}/{}".format(
+                        exchange.exchange_id, type(error).__name__, retries, max_retries
+                    )
+                )
+
+                if sleep_between_retry:
+                    sleep(1)
+
+        return decorated_function
+
+    return decorator
